cd39e655979375d38e71e207eb174fcb5dd0e329
The-Mag-Pie <barteksor2000@onet.pl>
2023-11-23 01:11:36 +0100

    Add currency table with last visited currencies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts">
+import { useRouter } from "vue-router"
 import TheWelcome from "../components/TheWelcome.vue"
+import currencies from "@/assets/currencies.json"
 
 document.title = "Kursy walut"
+
+const router = useRouter()
+
+const rowClicked = (code: string) => {
+    router.push({ name: "currency", params: { code } })
+}
+
+const lastVisited = JSON.parse(localStorage.getItem("lastVisited") ?? "[]") as Array<string>
+const lastVisitedCurrencies = lastVisited.map((code) => currencies.find((currency) => currency.code === code) ?? { code: "XXX", name: "Nieznana waluta" }).slice(0, 5)
 </script>
 
 <template>
     <main>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Lp.</th>
+                    <th scope="col">Waluta</th>
+                    <th scope="col">Kod</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="({ code, name }, index) in lastVisitedCurrencies" :key="code" @click="$event => rowClicked(code)">
+                    <th scope="row">{{ index + 1 }}</th>
+                    <td>{{ name }}</td>
+                    <td>{{ code }}</td>
+                </tr>
+            </tbody>
+        </table>
         <TheWelcome />
     </main>
 </template>
+
+<style scoped>
+tbody > tr {
+    cursor: pointer;
+}
+</style>
